805e11563323420a315dedd8fe3a5ad478532cc8
Carlos Rosquillas <crosquillas@gmail.com>
2017-09-15 14:39:36 -0700

    Agregar traducción de modulo

--- models/models.py
+++ models/models.py
@@ -5,4 +5,13 @@ from odoo import models, fields, api
 class campos_clientes_vittal(models.Model):
     _inherit = 'res.partner'
 
-    zip_extra = fields.Char(string="Zip Extra", help="Extended Zip code")
+    zip_extra = fields.Char(string="Zip Extra", help="Extended zip code")
+
+    # fn ex
+    #
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    #
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     self.value2 = float(self.value) / 100
