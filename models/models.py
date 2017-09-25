6a79ca2d97717cf22d1e55ef0e54d9fa082d4464
Carlos Rosquillas <crosquillas@gmail.com>
2017-09-25 14:48:07 -0700

    Autoformateo de python

--- models/models.py
+++ models/models.py
@@ -10,26 +10,19 @@ class user_client(models.Model):
 
     # direcciones del grupo
     address_ids = fields.One2many(
-        'partner.address',
-        'parent_id',
-        string="Addresses")
+        'partner.address', 'parent_id', string="Addresses")
 
     # miembros del grupo
     member_ids = fields.One2many(
-        'partner.member',
-        'parent_id',
-        string='Members')
+        'partner.member', 'parent_id', string='Members')
 
     # código de grupo
-    group_id = fields.Selection(string="Group Code",
-                                help="Internal group code",
-                                selection=[
-                                    ('area_p', 'AREA-P'),
-                                    ('conv', 'CONV'),
-                                    ('evento', 'EVENTO'),
-                                    ('grupos', 'GRUPOS'),
-                                    ('otros', 'OTROS')
-                                ])
+    group_id = fields.Selection(
+        string="Group Code",
+        help="Internal group code",
+        selection=[('area_p', 'AREA-P'), ('conv', 'CONV'),
+                   ('evento', 'EVENTO'), ('grupos', 'GRUPOS'), ('otros',
+                                                                'OTROS')])
 
     group_name = fields.Char(compute="_group_name", store=True)
 
@@ -49,32 +42,28 @@ class user_client(models.Model):
     zip_extra = fields.Char(string="Zip Extra", help="Extended zip code")
 
     # tipo de usuario adicional
-    client_type = fields.Selection(selection=[
-        ('company', 'Company'),
-        ('family', 'Family'),
-        ('individual', 'Individual')])
+    client_type = fields.Selection(selection=[('company', 'Company'),
+                                              ('family', 'Family'),
+                                              ('individual', 'Individual')])
 
     # nombre comercial
-    legal_name = fields.Char(string="Legal Name",
-                             help="Legal or fiscal name of business")
+    legal_name = fields.Char(
+        string="Legal Name", help="Legal or fiscal name of business")
 
     # rfc
     rfc = fields.Char(string="RFC", help="RFC Code")
 
     # lada de país
-    country_code = fields.Char(string="Country Code",
-                               help="Telephone country code")
+    country_code = fields.Char(
+        string="Country Code", help="Telephone country code")
 
     # código de zona
-    zone = fields.Selection(string="Zone", help="Zone code", selection=[
-        ('bqelms', 'BQELMS'),
-        ('itrlms', 'ITRLMS'),
-        ('lomas', 'LOMAS'),
-        ('plnco', 'PLNCO'),
-        ('sfe', 'SFE'),
-        ('tcmchl', 'TCMCHL'),
-        ('unica', 'UNICA')
-    ])
+    zone = fields.Selection(
+        string="Zone",
+        help="Zone code",
+        selection=[('bqelms', 'BQELMS'), ('itrlms', 'ITRLMS'),
+                   ('lomas', 'LOMAS'), ('plnco', 'PLNCO'), ('sfe', 'SFE'),
+                   ('tcmchl', 'TCMCHL'), ('unica', 'UNICA')])
 
     # código de producto
     product_custom_id = fields.Char(string="Product Code", help="Product code")
@@ -85,8 +74,8 @@ class user_client(models.Model):
     # fechas de suscripción
     sub_duration = fields.Integer('Duration', default=1)
 
-    sub_start_date = fields.Date(string="Start of Subscription",
-                                 help="Date when subscription started")
+    sub_start_date = fields.Date(
+        string="Start of Subscription", help="Date when subscription started")
 
     sub_end_date = fields.Date(compute="_add_month", store=True)
 
@@ -99,9 +88,8 @@ class user_client(models.Model):
                 record.sub_end_date = fields.Datetime.to_string(endtime)
 
     # hogar protegido
-    protected_home_auto = fields.Boolean(compute="_protected_home_auto",
-                                         store=True,
-                                         readonly=True)
+    protected_home_auto = fields.Boolean(
+        compute="_protected_home_auto", store=True, readonly=True)
 
     @api.depends('quantity')
     def _protected_home_auto(self):
@@ -109,8 +97,8 @@ class user_client(models.Model):
             if record.quantity > 4:
                 record.protected_home_auto = True
 
-    protected_home_user = fields.Boolean(string='Protected Home',
-                                         help="Protected home program")
+    protected_home_user = fields.Boolean(
+        string='Protected Home', help="Protected home program")
 
     protected_home = fields.Boolean(compute="_protected_home", store=True)
 
@@ -127,14 +115,11 @@ class partner_member(models.Model):
 
     # número de socio
     member_id = fields.Char(
-        string="Associate ID",
-        help="Unique identifier for associates")
+        string="Associate ID", help="Unique identifier for associates")
 
     # padre del miembro
     parent_id = fields.Many2one(
-        'res.partner',
-        string="Parent",
-        ondelete="set null")
+        'res.partner', string="Parent", ondelete="set null")
 
     # titular del grupo
     is_owner = fields.Boolean(string="Owner", help="Is this the owner")
@@ -143,12 +128,8 @@ class partner_member(models.Model):
     relationship = fields.Selection(
         string="Relationship",
         help="Type of member",
-        selection=[
-            ("1", "Spouse"),
-            ("3", "Offspring"),
-            ("4", "Other family"),
-            ("9", "Not family")]
-    )
+        selection=[("1", "Spouse"), ("3", "Offspring"), ("4", "Other family"),
+                   ("9", "Not family")])
 
     # fecha de nacimiento
     birthday = fields.Date(string="Birthday", help="Date of birth")
@@ -160,37 +141,35 @@ class partner_address(models.Model):
 
     # padre de la dirección
     parent_id = fields.Many2one(
-        'res.partner',
-        string="Parent",
-        ondelete="set null")
+        'res.partner', string="Parent", ondelete="set null")
 
     # tipos de dirección
     address_type = fields.Selection(
         string="Address Type",
         help="Group addresses",
-        selection=[
-            ('admin', odoo._('Administrative Address')),
-            ('fiscal', odoo._('Fiscal Address')),
-            ('coverage', odoo._('Coverage Address')),
-            ('attention', odoo._('Support Address'))
-        ])
+        selection=[('admin', odoo._('Administrative Address')),
+                   ('fiscal', odoo._('Fiscal Address')),
+                   ('coverage',
+                    odoo._('Coverage Address')), ('attention',
+                                                  odoo._('Support Address'))])
 
     # entre calles
-    cross_street = fields.Char(string="Cross Streets",
-                               help="Nearest intersection")
+    cross_street = fields.Char(
+        string="Cross Streets", help="Nearest intersection")
 
     # referencias
-    references = fields.Char(string="References",
-                             help="Nearby landmarks or reference points")
+    references = fields.Char(
+        string="References", help="Nearby landmarks or reference points")
 
     # fachada
-    exterior = fields.Char(string="Exterior",
-                           help="Description of exterior of address")
+    exterior = fields.Char(
+        string="Exterior", help="Description of exterior of address")
 
     # características especiales
-    details = fields.Char(string="Details",
-                          help="Special details or characteristics")
+    details = fields.Char(
+        string="Details", help="Special details or characteristics")
 
     # establecer como domicilio de atención default
-    default_address = fields.Boolean(string="Default Address",
-                                     help="Default address for the group")
+    default_address = fields.Boolean(
+        string="Default Address", help="Default address for the group")
+
