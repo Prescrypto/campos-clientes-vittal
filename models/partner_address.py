b20778358bc08ced7ee6b289603cde44e8433651
Carlos Rosquillas <crosquillas@gmail.com>
2017-09-26 13:24:38 -0700

    Estandarizar formato de archivos

--- models/partner_address.py
+++ models/partner_address.py
@@ -5,39 +5,33 @@ from odoo import models, fields
 
 
 class partner_address(models.Model):
-    _name = 'partner.address'
-    _inherit = 'res.partner'
+    _name = "partner.address"
+    _inherit = "res.partner"
 
     # padre de la dirección
     parent_id = fields.Many2one(
-        'res.partner', string="Parent", ondelete="set null")
+        "res.partner", string="Parent", ondelete="set null")
 
     # tipos de dirección
     address_type = fields.Selection(
         string="Address Type",
-        help="Group addresses",
-        selection=[('admin', odoo._('Administrative Address')),
-                   ('fiscal', odoo._('Fiscal Address')),
-                   ('coverage',
-                    odoo._('Coverage Address')), ('attention',
-                                                  odoo._('Support Address'))])
+        selection=[("admin", odoo._("Administrative Address")),
+                   ("fiscal", odoo._("Fiscal Address")),
+                   ("coverage",
+                    odoo._("Coverage Address")), ("attention",
+                                                  odoo._("Support Address"))])
 
     # entre calles
-    cross_street = fields.Char(
-        string="Cross Streets", help="Nearest intersection")
+    cross_street = fields.Char("Cross Streets")
 
     # referencias
-    references = fields.Char(
-        string="References", help="Nearby landmarks or reference points")
+    references = fields.Char("References")
 
     # fachada
-    exterior = fields.Char(
-        string="Exterior", help="Description of exterior of address")
+    exterior = fields.Char("Exterior")
 
     # características especiales
-    details = fields.Char(
-        string="Details", help="Special details or characteristics")
+    details = fields.Char("Details")
 
     # establecer como domicilio de atención default
-    default_address = fields.Boolean(
-        string="Default Address", help="Default address for the group")
+    default_address = fields.Boolean("Default Address")
--- models/partner_member.py
+++ models/partner_member.py
@@ -4,26 +4,24 @@ from odoo import models, fields
 
 
 class partner_member(models.Model):
-    _name = 'partner.member'
-    _inherit = 'res.partner'
+    _name = "partner.member"
+    _inherit = "res.partner"
 
     # número de socio
-    member_id = fields.Char(
-        string="Associate ID", help="Unique identifier for associates")
+    member_id = fields.Char("Associate ID")
 
     # padre del miembro
     parent_id = fields.Many2one(
-        'res.partner', string="Parent", ondelete="set null")
+        "res.partner", string="Parent", ondelete="set null")
 
     # titular del grupo
-    is_owner = fields.Boolean(string="Owner", help="Is this the owner")
+    is_owner = fields.Boolean("Owner")
 
     # condición de grupo
     relationship = fields.Selection(
         string="Relationship",
-        help="Type of member",
         selection=[("1", "Spouse"), ("3", "Offspring"), ("4", "Other family"),
                    ("9", "Not family")])
 
     # fecha de nacimiento
-    birthday = fields.Date(string="Birthday", help="Date of birth")
+    birthday = fields.Date("Birthday")
