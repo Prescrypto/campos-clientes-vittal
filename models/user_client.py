f880ced4ee408456e88f6ea0badef855eadc82c8
Carlos Rosquillas <crosquillas@gmail.com>
2017-10-04 23:41:15 -0700

    F1ST-234 Agregar campos para la creación de Grupos Familiares

--- models/user_client.py
+++ models/user_client.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import odoo
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class user_client(models.Model):
     _inherit = "res.partner"
+
+    # titular
+    main_contact = fields.Char("Main Contact")
+
+    # codigo de grupo
+    group_code = fields.Char("Group Code", compute="_group_code", store=True)
+
+    @api.one
+    @api.depends("client_type")
+    def _group_code(self):
+        if self.client_type and self.id:
+            prefix = {
+                "company": "E",
+                "family": "F",
+                "private": "P",
+            }[self.client_type]
+            self.group_code = prefix + str(self.id)
 
     # direcciones del grupo
     address_ids = fields.One2many(
@@ -19,35 +35,13 @@ class user_client(models.Model):
     company_ids = fields.One2many(
         "company.member", "parent_id", string="Company Members")
 
-    # código de grupo
-    group_id = fields.Selection(
-        string="Group Code",
-        selection=[("area_p", "AREA-P"), ("conv", "CONV"),
-                   ("evento", "EVENTO"), ("grupos", "GRUPOS"), ("otros",
-                                                                "OTROS")])
-
-    group_name = fields.Char(compute="_group_name", store=True)
-
-    @api.depends("group_id")
-    def _group_name(self):
-        for record in self:
-            if record.group_id:
-                record.group_name = {
-                    "area_p": odoo._("Protected Areas"),
-                    "conv": odoo._("Agreements"),
-                    "evento": odoo._("Events"),
-                    "grupos": odoo._("Groups"),
-                    "otros": odoo._("Other")
-                }.get(record.group_id, odoo._("None Selected"))
-
     # código postal de colonia
     zip_extra = fields.Char("Zip Extra")
 
     # tipo de usuario adicional
-    client_type = fields.Selection(selection=[("company", odoo._("Company")),
-                                              ("family", odoo._("Family")),
-                                              ("individual",
-                                               odoo._("Individual"))])
+    client_type = fields.Selection(selection=[("company", _("Company")),
+                                              ("family", _("Family")),
+                                              ("private", _("Private"))])
 
     # nombre comercial
     legal_name = fields.Char("Legal Name")
@@ -55,39 +49,9 @@ class user_client(models.Model):
     # rfc
     rfc = fields.Char("RFC")
 
-    # lada de país
-    country_code = fields.Char("Country Code")
-
     # código de zona
     zone = fields.Selection(
         string="Zone",
         selection=[("bqelms", "BQELMS"), ("itrlms", "ITRLMS"),
                    ("lomas", "LOMAS"), ("plnco", "PLNCO"), ("sfe", "SFE"),
                    ("tcmchl", "TCMCHL"), ("unica", "UNICA")])
-
-    # código de producto
-    product_custom_id = fields.Char("Product Code")
-
-    # cápitas
-    quantity = fields.Integer("Quantity")
-
-    # hogar protegido
-    protected_home_auto = fields.Boolean(
-        compute="_protected_home_auto", store=True, readonly=True)
-
-    @api.depends("quantity")
-    def _protected_home_auto(self):
-        for record in self:
-            if record.quantity > 4:
-                record.protected_home_auto = True
-
-    protected_home_user = fields.Boolean("Protected Home")
-
-    protected_home = fields.Boolean(compute="_protected_home", store=True)
-
-    @api.depends("protected_home_auto", "protected_home_user")
-    def _protected_home(self):
-        for record in self:
-            if record.protected_home_auto or record.protected_home_user:
-                record.protected_home = True
-
