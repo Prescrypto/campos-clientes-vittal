1c859daac256a7e40055f1278fa42dbe62964345
Antonio Silva <gsisa.asilva@hotmail.com>
2018-05-01 23:41:35 -0500

    Se aisla la funcionalidad de exportacion y de subscripción.

--- models/user_client.py
+++ models/user_client.py
@@ -160,48 +160,7 @@ class user_client(models.Model):
 
     # id externo en tabla de res_partner
     client_export_id = fields.Char("Export ID", default="None")
-
-    # exportación sae
-    export_columns = [
-        'client_export_id',
-        'name',
-        'rfc',
-        'street',
-        'street2',
-        'cross_street',
-        'crosses_with',
-        'sat_colonia_id',
-        'zip',
-        'poblacion',
-        'sat_municipio_id',
-        'sat_estado_id',
-        'sat_pais_id',
-        'nacionalidad',
-        'reference_id',
-        'phone',
-        'fax',
-        'website',
-        'curp',
-        'invoice_email',
-        'sat_uso_codigo',
-        'sat_pagos_codigo',
-        'zone',
-    ]
-    @api.multi
-    def export(self):
-        columns = self.export_columns
-        format_clients = partial(sae.format, 'clients')
-        return map(format_clients, self.export_data(columns).get('datas', []))
-
-    def export_all(self):
-        all_clients = self.env['res.partner']
-        valid_clients = all_clients.search([['active', '=', True], ['customer', '=', True], ['parent_id', '=', False]])
-
-        columns = self.export_columns
-        format_clients = partial(sae.format, 'clients')
-
-        return map(format_clients, valid_clients.export_data(columns).get('datas', []))
-
+    
     def name_get(self):
         ''' Cambiar formato de nombre de titular y direcciones '''
         result = []
