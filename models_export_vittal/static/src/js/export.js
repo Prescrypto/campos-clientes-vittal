c94d86405cde279d6c1c9a712124b1d83cfaeb67
valerybriz <valerybriz@gmail.com>
2018-11-13 16:16:33 -0600

    add new js function and logs

--- models_export_vittal/static/src/js/export.js
+++ models_export_vittal/static/src/js/export.js
@@ -25,7 +25,6 @@ openerp.models_export_vittal = function(instance, local) {
         .done(createCsv.bind(this, filename, type));
     },
     export_all_button: function(event) {
-      console.log(extractIds(this.records.records));
       var type = event.target.dataset.type;
       var filename = event.target.dataset.filename + '.completo.csv';
       new instance.web.Model(this.model)
@@ -38,9 +37,54 @@ openerp.models_export_vittal = function(instance, local) {
 };
 
 function extractIds(records) {
-  console.log(_.map(records, record => record.attributes.name));
-  console.log(_.map(records, record => record.attributes.attrs));
-  return _.map(records, record => record.attributes.id);
+   return _.map(records, record => record.attributes.id);
+}
+
+function get_checked_rows(){
+    console.log("get checked");
+     var self = this;
+	 view = this.getParent();
+	 children = view.getChildren();
+
+     var export_columns_keys = [];
+     var export_columns_names = [];
+     var columns_tab = [];
+     var rows_tab = [];
+     console.log("get ths");
+    // find the first tr of table for the columns labels
+         view.$el.find('th.o_list_record_selector ').closest('tr').each(function (i, el) {
+                 var ihtml = el.innerHTML + ''; // get html content of the tr
+                 var extractedTdArray = ihtml.split("</th>") // split tr in array of th
+
+            // get the text content of each th
+                 extractedTdArray.forEach(function(elt){
+                     var val = elt.substring(elt.indexOf(">") + 1)
+                     if(val){
+                         columns_tab.push(val)
+                     }
+                 });
+
+         });
+
+        console.log("get tds");
+         var export_rows = [];
+            // find the all ckecked rows
+         view.$el.find('td.o_list_record_selector input:checked').closest('tr').each(function (i, el) {
+                 rows_tab = []
+                 var ihtml = el.innerHTML + '';
+                 var extractedData = ihtml.split("</td>")
+                 extractedData.forEach(function(elt){
+                     var val = elt.substring(elt.indexOf(">") + 1) // get the td content
+                     if(val != undefined){
+                         if(val.indexOf(">") < 0 ){ // if the content of the td is not html content
+                             rows_tab.push(val)
+                         }
+                     }
+                 });
+                 export_rows.push(rows_tab)
+         });
+
+    console.log(export_rows);
 }
 
 function createCsv(filename, type, source) {
