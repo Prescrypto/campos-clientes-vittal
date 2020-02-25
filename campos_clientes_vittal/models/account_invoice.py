a737fd225e8324c20f7151858ef28d1c3efd9be0
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2020-02-24 19:06:41 -0600

    add source action invoice open def to local definitions to add sat cfdi logic

--- campos_clientes_vittal/models/account_invoice.py
+++ campos_clientes_vittal/models/account_invoice.py
@@ -23,3 +23,13 @@ class Invoice(models.Model):
         self.sat_uso_id = self.partner_id.fiscal_address.sat_uso_id
         self.sat_metodo_pago = self.partner_id.fiscal_address.sat_metodo_pago
         self.sat_pagos_id = self.partner_id.fiscal_address.sat_pagos_id
+
+    @api.multi
+    def action_invoice_open(self):
+        # lots of duplicate calls to action_invoice_open, so we remove those already open
+        to_open_invoices = self.filtered(lambda inv: inv.state != 'open')
+        if to_open_invoices.filtered(lambda inv: inv.state not in ['proforma2', 'draft']):
+            raise UserError(_("Invoice must be in draft or Pro-forma state in order to validate it."))
+        to_open_invoices.action_date_assign()
+        to_open_invoices.action_move_create()
+        return to_open_invoices.invoice_validate()
